44cb0ca4183f2b6f2bd4781afbda75544e35e22b
JCDMeira <jean.meira10@hotmail.com>
2021-04-20 11:10:45 -0300

    Atualiza componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,19 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './styles.module.scss';
+
 export function Header(){
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    });
+
     return(
-        <header>
-            
+        <header className={styles.headerContainer}>
+            <img src="logo.svg" alt="Podcastr"/>
+
+            <p>O melhor para ouvir, sempre</p>
+            <span>{currentDate}</span>
         </header>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { Header } from "../components/Header";
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <Header/>
   )
 }
